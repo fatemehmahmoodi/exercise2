e7d6f66978d0bf7d10639ad8a479b6086215406c
fatemehmahmoodi <116880900+fatemehmahmoodi@users.noreply.github.com>
2022-11-03 20:54:27 +0330

    Add files via upload

---
+++ pro1.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+
+
+    float ans=0;
+    float y;
+    cout<<"enter your Temp as Farenheit:";
+    cin>>y;
+    cout<<"\n Ur temp as Celsuis:";
+    ans=(5.0/9.0)*(y-32);
+    cout<<ans<<endl;
+
+}
